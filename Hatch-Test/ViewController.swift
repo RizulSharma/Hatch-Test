a473c9f3db153f76e46446a9c189e82150d83c76
RizulSharma <rizul@nexgsolutions.com>
2022-03-30 14:31:27 +0530

    -Initial Commit - Added the sphere and the box. - Added camera access.

--- Hatch-Test/ViewController.swift
+++ Hatch-Test/ViewController.swift
@@ -13,20 +13,15 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Set the view's delegate
         sceneView.delegate = self
-        
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
-        
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
+
+        createMarsNBox()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -34,8 +29,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
-
-        // Run the view's session
+        //This will print if the current device supports world tracking or not.
+        debugPrint("World tracking supported: ", ARWorldTrackingConfiguration.isSupported)
         sceneView.session.run(configuration)
     }
     
@@ -45,30 +40,41 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Pause the view's session
         sceneView.session.pause()
     }
+    
+    fileprivate func createMarsNBox() {
+        let node1 = SCNNode()
+        node1.position = SCNVector3(0, -0.1, -0.5)
+        node1.geometry = self.createBox()
+        
+        let node2 = SCNNode()
+        node2.position = SCNVector3(0.1, 0.1, -0.5)
+        node2.geometry = self.createSphere()
+        
+        sceneView.scene.rootNode.addChildNode(node1)
+        node1.addChildNode(node2)
+        sceneView.autoenablesDefaultLighting = true
+    }
+    
+    
+    /// Creates a box geometry in 3dspace.
+    fileprivate func createBox()-> SCNBox {
+        //Creating a box. l/b/h is in meters here.
+        let box = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.0)
+        let material = SCNMaterial()
+        material.diffuse.contents = UIColor.red
+        box.materials = [material]
+        return box
+    }
+    
+    /// Creates a box geometry in 3dspace.
+    fileprivate func createSphere()-> SCNSphere {
+        //Creating a sphere. l/b/h is in meters here.
+        let sphere = SCNSphere(radius: 0.2)
+        let material = SCNMaterial()
+        // Used mars image just for visual enhancement.
+        material.diffuse.contents = UIImage(named: "art.scnassets/8k_mars.jpeg")
+        sphere.materials = [material]
+        return sphere
+    }
 
-    // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
-    }
 }
